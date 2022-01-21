0501b5b2258b710e6d21aca9612b609485343469
saul zarate <saul.zarate@davinci.edu.ar>
2022-01-21 18:26:20 -0300

    Maquetación completa (con responsive)

--- assets/app.js
+++ assets/app.js
@@ -8,33 +8,78 @@ window.addEventListener('DOMContentLoaded', () => {
     const div_filter = document.querySelector('.content_select div')
     const options_select = document.querySelectorAll('#ul_select > li')
 
-    div_content_mode.addEventListener('click',(e) => switchMode(e.target))
+    div_content_mode.addEventListener('click', (e) => switchMode(e.target))
     div_filter.addEventListener('click', (e) => activeAccordion(div_filter))
-    options_select.forEach(option => option.addEventListener('click', e  => addFilterSelect(e.target)))
+    options_select.forEach(option => option.addEventListener('click', e => addFilterSelect(e.target)))
+    /* Div to Input */
+    divToInput()
+
+
+    /* DATA - API */
+    let data = getDataLocalStorage()
+
+    if (data == null ){
+        // Find in API
+        fetch(URL_API)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                // save
+                saveLocalStorage(data)
+
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }else{
+        console.log(getDataLocalStorage())
+    }
+
+
 })
 
-function activeAccordion(element){
-    if(element.classList.contains('accordion-none')){
+function activeAccordion(element) {
+    if (element.classList.contains('accordion-none')) {
         element.classList.remove('accordion-none')
         element.lastElementChild.classList.remove('d-none')
-    }else{
+    } else {
         element.classList.add('accordion-none')
         element.lastElementChild.classList.add('d-none')
     }
 }
-function switchMode(element){
+function switchMode(element) {
     const p_dark = document.getElementById('mode-dark')
     const p_light = document.getElementById('mode-light')
 
-    if(element.id == 'mode-dark' ){
+    if (element.id == 'mode-dark') {
         p_light.classList.remove('active')
         p_dark.classList.add('active')
     }
-    if(element.id == 'mode-light' ){
+    if (element.id == 'mode-light') {
         p_dark.classList.remove('active')
         p_light.classList.add('active')
     }
 }
-function addFilterSelect(option){
+function addFilterSelect(option) {
     console.log(option.textContent)
+}
+function divToInput(){
+    const input = document.getElementById('input_countrie')
+    const contentSearch = document.querySelector('section.content_filters .content_search')
+    
+    contentSearch.addEventListener('click', () => input.focus())
+    
+}
+
+
+/* DATA */
+function saveLocalStorage(data) {
+    const dataObject = {
+        length: data.length,
+        data
+    }
+    localStorage.setItem('data', JSON.stringify(dataObject))
+}
+function getDataLocalStorage() {
+    return JSON.parse(localStorage.getItem('data'))
 }
