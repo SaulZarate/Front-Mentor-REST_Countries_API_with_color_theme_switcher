d77e7f04ca30c244e09f2a4d41cabc51af719268
saul zarate <saul.zarate@davinci.edu.ar>
2022-01-22 14:33:09 -0300

    Cards con paises agregados

--- assets/app.js
+++ assets/app.js
@@ -7,36 +7,38 @@ window.addEventListener('DOMContentLoaded', () => {
     const div_content_mode = document.getElementById('content_mode')
     const div_filter = document.querySelector('.content_select div')
     const options_select = document.querySelectorAll('#ul_select > li')
+    const div_countries = document.querySelector('section.content_countries')
 
     div_content_mode.addEventListener('click', (e) => switchMode(e.target))
     div_filter.addEventListener('click', (e) => activeAccordion(div_filter))
     options_select.forEach(option => option.addEventListener('click', e => addFilterSelect(e.target)))
-    /* Div to Input */
-    divToInput()
+    focusInputSearch()
 
 
     /* DATA - API */
     let data = getDataLocalStorage()
-
     if (data == null ){
         // Find in API
         fetch(URL_API)
             .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                // save
-                saveLocalStorage(data)
-
+            .then(info => {
+                console.log(info)
+                // Save
+                saveLocalStorage(info)
+                // Add countries to DOM
+                div_countries.innerHTML = createHTMLCountries(info)
             })
             .catch(err => {
                 console.log(err)
             })
-    }else{
-        console.log(getDataLocalStorage())
+        }else{
+            // Add countries to DOM
+            div_countries.innerHTML = createHTMLCountries(data.data)
     }
 
 
 })
+
 
 function activeAccordion(element) {
     if (element.classList.contains('accordion-none')) {
@@ -63,7 +65,7 @@ function switchMode(element) {
 function addFilterSelect(option) {
     console.log(option.textContent)
 }
-function divToInput(){
+function focusInputSearch(){
     const input = document.getElementById('input_countrie')
     const contentSearch = document.querySelector('section.content_filters .content_search')
     
@@ -82,4 +84,32 @@ function saveLocalStorage(data) {
 }
 function getDataLocalStorage() {
     return JSON.parse(localStorage.getItem('data'))
+}
+
+
+/* HTML Countries */
+function createHTMLCountries(countries){
+    let htmlCountries = ''
+
+    countries.forEach( countrie => {
+        htmlCountries += createCardCountrie(countrie)
+    })
+
+    return htmlCountries
+}
+/* Create Card */
+function createCardCountrie(countrie){
+    return `
+    <div class="card">
+            <div class="content_image">
+                <img src="${countrie.flags.png}" alt="${countrie.name}">
+            </div>
+            <div class="content_text">
+                <h2>${countrie.name}</h2>
+                <p><span>Population:</span> ${countrie.population}</p>
+                <p><span>Region:</span> ${countrie.region}</p>
+                <p><span>Capital:</span> ${countrie.capital}</p>
+            </div>
+        </div>
+    `
 }
